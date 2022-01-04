0a4339235218aa048b17119dd8eded61aafffc61
zjr <615817413@qq.com>
2022-01-04 09:01:20 +0000

    feat: init model

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import numpy as np
 import yaml
 import random
 from models.colorization_net import *
+import torch
 
 # Log Settings
 logging.basicConfig(level = logging.INFO,format = '[%(asctime)s] [%(name)s] [%(levelname)s] %(message)s')
@@ -35,12 +36,17 @@ def init_seeds(seed=0, cuda_deterministic=True):
         torch.backends.cudnn.deterministic = False
         torch.backends.cudnn.benchmark = True
 
+def init_model(is_train=True):
+    model = ColorizationNet(cfgs)
+    model.train(is_train)
+    model = torch.nn.DataParallel(model)
+    return model
+
 def train(cfgs):
     # Fix random seed
     init_seeds(0)
     # Init model
-    model = ColorizationNet(cfgs)
-    model.train()
+    model = init_model()
     # Init data loader
     train_loader = get_data_loader('./data', cfgs['batch_size'], True)
     # Define train step
@@ -63,9 +69,9 @@ def train(cfgs):
     max_epoch = cfgs['epoch']
     log_step = cfgs['log_step']
     for epoch in range(max_epoch):
-        total_step = len(model.train_loader)
+        total_step = len(train_loader)
         loss_cnt = col_loss_cnt = cls_loss_cnt = 0
-        for i, ipts in enumerate(model.train_loader, start=1):
+        for i, ipts in enumerate(train_loader, start=1):
             L, ab, cls_gt = ipts # ipts[3]: L[b,1,h,w], ab[b,2,h,w], lab[b]
             ab_out, cls_out = model(L) # ab_out[b,2,h,w], lab_out[b, class_nums]
             loss, colorization_loss, classification_loss = loss(ab, ab_out, cls_gt, cls_out)
