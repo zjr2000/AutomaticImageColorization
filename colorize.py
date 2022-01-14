da233562f2587cd77fa4491eb07717299b4375b8
zjr <615817413@qq.com>
2022-01-14 02:12:48 +0000

    fix: colorize failed

--- colorize.py
+++ colorize.py
@@ -40,28 +40,14 @@ def net_out2rgb(L, ab_out):
         3 channel RGB image
     """
     # Convert to numpy and unnnormalize
-    L = L.cpu().numpy() * 100.0
-    ab_out = ab_out.cpu().numpy() * 254.0 - 127.0
+    Lab = torch.cat([L, ab_out]).cpu().numpy()
     
-    
-    # L and ab_out are tenosr i.e. are of shape of
-    # Height x Width x Channels
-    # We need to transpose axis back to HxWxC
-    L = L.transpose((1, 2, 0))
-    ab_out = ab_out.transpose((1, 2, 0))
+    Lab = Lab.transpose((1, 2, 0))
+    Lab[:,:,:1] = Lab[:,:,:1] * 100.0
+    Lab[:,:,1:] = Lab[:,:,1:] * 256.0 - 127.0
 
-    # Stack layers  
-    img_stack = np.dstack((L, ab_out))
-    
-    # This line is CRUCIALL
-    #   - torch requires tensors to be float32
-    #   - thus all above (L, ab) are float32
-    #   - scikit image floats are in range -1 to 1
-    #   - http://scikit-image.org/docs/dev/user_guide/data_types.html
-    #   - idk what's next, but converting image to float64 somehow
-    #     does the job. scikit automagically converts those values.
-    img_stack = img_stack.astype(np.float64)	
-    return  color.lab2rgb(img_stack)
+    Lab = Lab.astype(np.float64)	
+    return  color.lab2rgb(Lab) * 255.0
 
 
 def colorize_test_set(cfgs):
@@ -69,9 +55,9 @@ def colorize_test_set(cfgs):
     if not os.path.exists(pred_dir):
         os.mkdir(pred_dir)
     model = load_model(cfgs)
-    eval_loader = get_data_loader('./data', 1, False)
+    eval_loader = get_data_loader('./data', 1, False, shuffle=False)
     with torch.no_grad():
-        for i, ipts in tqdm(enumerate(eval_loader, start=1)):
+        for i, ipts in tqdm(enumerate(eval_loader)):
             file_name = 'img_pred' + str(i) +'.png'
             L, ab, _ = ipts
             L = L.to(DEVICE)
