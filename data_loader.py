ac567f3a2e41ff7895e9ff88318929ab407116fa
zjr <615817413@qq.com>
2022-01-15 10:22:49 +0000

    use ImageFolder as data loader to adapt places365 dataset

--- data_loader.py
+++ data_loader.py
@@ -1,32 +1,48 @@
-import skimage
 import torchvision.transforms as transforms
-from torchvision.datasets.cifar import CIFAR100
+from torchvision.datasets import ImageFolder
 import torch
 from skimage import color, io
+from skimage.transform import resize
 import numpy as np
+import os
+
+class HandleGray(object):
+    def __call__(self, image):
+        if len(image.shape) < 3:
+            image = color.gray2rgb(image)
+        return image
+
 
 class Rgb2Lab(object):
     def __call__(self, image):
-        image = image.permute(1, 2, 0).contiguous()
         assert image.shape == (224, 224, 3)
         img_lab = color.rgb2lab(image)
         img_lab[:,:,:1] = img_lab[:,:,:1] / 100.0
         img_lab[:,:,1:] = (img_lab[:,:,1:] + 128.0) / 256.0
         return img_lab
 
+
 class SplitLab(object):
     def __call__(self, image):
-        image = image.transpose(2, 0, 1)
         assert image.shape == (3, 224, 224)
         L  = image[:1,:,:]
         ab = image[1:,:,:]
         return (L, ab)
 
-class DataSet(CIFAR100):
-    def __init__(self, root, train=True, transform=None, target_transform=None, download=False):
-        super().__init__(root, train=train, transform=transform, target_transform=target_transform, download=download)
+
+class Resize(object):
+    def __init__(self, size):
+        self.size = (size, size)
+    
+    def __call__(self, image):
+        return resize(image, self.size)
+
+
+class DataSet(ImageFolder):
+    def __init__(self, root, transform=None, target_transform=None):
+        super().__init__(root, transform=transform, target_transform=target_transform, loader=io.imread)
         self.composed = transforms.Compose(
-            [transforms.Resize(224), transforms.ToTensor(), Rgb2Lab(), SplitLab()]
+            [HandleGray(), Resize(224), Rgb2Lab(), transforms.ToTensor(), SplitLab()]
         )
     def __getitem__(self, index):
         image, label = super().__getitem__(index) # image: [c, h, w]
@@ -35,7 +51,9 @@ class DataSet(CIFAR100):
 
 
 def get_data_loader(root, batch_size, isTrain, num_workers=1, shuffle=True):
-    dataset = DataSet(root, train=isTrain, download=False)
+    image_dir = 'train' if isTrain else 'test'
+    root = os.path.join(root, image_dir)
+    dataset = DataSet(root)
     return torch.utils.data.DataLoader(
         dataset, 
         batch_size=batch_size, 
@@ -44,7 +62,7 @@ def get_data_loader(root, batch_size, isTrain, num_workers=1, shuffle=True):
     )
 
 if __name__ == '__main__':
-    loader = get_data_loader('./data', 1, True)
+    loader = get_data_loader('./data/places10', 1, True)
     d = next(iter(loader))
     print(d[0])
     print(d[1])
